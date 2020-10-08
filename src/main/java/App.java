22ef51c5dc2c668a8919986ec050273c0a13e437
sars33 <sergetester33@mail.com>
2020-10-08 18:07:12 +0300

    Java pre-project. Practical task 2.1.2

---
+++ src/main/java/App.java
@@ -0,0 +1,24 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        HelloWorld secondbean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catBean =
+                (Cat) applicationContext.getBean("cat");
+        Cat secondCatBean =
+                (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean.equals(secondbean));
+        System.out.println(catBean.equals(secondCatBean));
+
+    }
+}
